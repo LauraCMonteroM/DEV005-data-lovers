2e4c4ea75b5f2a3af676e4230d7754dfe70b1d31
unknown <lauramontero205@gmail.com>
2023-03-08 16:25:53 -0500

    agrega botones filtrar y ordenar en html y muestra el nombre de los personajes con un bucle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/harrypotter/harry.js";
 
+const container = document.getElementById("temp-characters");
+//console.log(container);
+for (let i = 0; i < data.characters.length; i++) {
+  const containerCharacter = document.createElement("div");
+  //console.log(i)
+  container.appendChild(containerCharacter);
+  containerCharacter.innerHTML ="<p>" + data.characters[i].name + "<p/>";
+  containerCharacter.className = "characterBox"
+  console.log(containerCharacter.outerHTML);
+}
