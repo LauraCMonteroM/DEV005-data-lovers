2b23583374a937bbe1e7af40b9e2d8398b6beb45
LauraCMonteroM <lauramontero205@gmail.com>
2023-03-22 13:27:20 -0500

    mejora el coverage de los test, habilita el header para retornar a la pantalla principal

--- src/main.js
+++ src/main.js
@@ -4,33 +4,41 @@ import { ordenarAlfabeticamente, filtrar, calcular } from "./data.js";
 const dataCharacters = dataHarryPotter.characters;
 const dataBooks = dataHarryPotter.books;
 const dataFacts = dataHarryPotter.funFacts;
-const container = document.getElementById("temp-characters");
-const contenidoPrincipal = document.getElementById("sectionMostrando");
-const imagenPrincipal = document.getElementById("imgPrincipal");
 const charactersSection = document.getElementById("temp-characters");
 const booksSection = document.getElementById("bookssection");
+const funFactssection = document.getElementById("funFactssection");
+const contenidoPrincipal = document.getElementById("sectionMostrando");
+const imagenPrincipal = document.getElementById("imgPrincipal");
 const books = document.getElementById("books");
 const imgHeader = document.getElementById("header-img");
 const navBarTexts = document.getElementById("sections");
 const buttons = document.getElementById("btns");
 const sectionTitle = document.getElementById("titleSection");
-const facts = document.getElementById('funfacts')
-const funFactssection = document.getElementById('funFactssection');
-const btncalcular = document.getElementById('calc');
-
+const facts = document.getElementById("funfacts");
+const btncalcular = document.getElementById("calc");
+const calculatedFunfact = document.getElementById("funFactnswer");
 
 //CARDS CHARACTERS
+
+const imgHouses = {
+  Gryffindor: "./img/Gryffindor.png",
+  Hufflepuff: "./img/Hufflepuff.jpg",
+  Ravenclaw: "./img/Ravenclaw.jpg",
+  Slytherin: "./img/Slytherin.jpg",
+  null: 
+};
+
 function template(card) {
   let fragment = "";
   card.forEach((element) => {
     fragment += `
 <div class="cards">
-<img src="./img/Ppersonaje.png" class="imgCharacter">
-<p class="objetname">${element.name}</p>
+  <img src="${imgHouses[element.house]}" class="imgHouse" >
+  <p class="objetname">${element.name}</p>
 </div>
 `;
   });
-  container.innerHTML = fragment;
+  charactersSection.innerHTML = fragment;
 }
 template(dataCharacters);
 
@@ -38,14 +46,14 @@ template(dataCharacters);
 document.querySelector(".btn-ordenar").addEventListener("change", function (x) {
   if (x.target.value === "A-Z") {
     const resultAZ = ordenarAlfabeticamente(dataCharacters);
-    container.innerHTML = "";
+    charactersSection.innerHTML = "";
     template(resultAZ);
   } else if (x.target.value === "Z-A") {
-    container.innerHTML = "";
+    charactersSection.innerHTML = "";
     const resultZA = ordenarAlfabeticamente(dataCharacters).reverse();
     template(resultZA);
   } else if (x.target.value === "ordenar") {
-    container.innerHTML = "";
+    charactersSection.innerHTML = "";
     template(dataCharacters);
   }
 });
@@ -57,7 +65,14 @@ document.querySelector(".btn-filtrar").addEventListener("change", function (e) {
   template(data);
 });
 
-
+//CALCULATE
+btncalcular.addEventListener("click", function () {
+  const data = calcular(dataCharacters, "Female");
+  const h3 = '<h3 class= "typeFunFact1">Sabías qué...</h3>';
+  const imgwomen = '<img src="./img/datocuriosomujeres.png" class="imgFact1">';
+  calculatedFunfact.style.display = "block";
+  calculatedFunfact.innerHTML = h3 + imgwomen + "Existen " + data + " personajes femeninos en la saga?";
+});
 
 //CHANGE TO s.LIBROS
 function templateBooks(cardbooks) {
@@ -80,17 +95,16 @@ books.addEventListener("click", function (e) {
   imagenPrincipal.style.display = "none";
   buttons.style.display = "none";
   funFactssection.style.display = "none";
-  btncalcular.style.display = "none"
+  btncalcular.style.display = "none";
   booksSection.style.display = "block";
   imgHeader.style.display = "block";
   imgHeader.style.marginTop = "0px";
   navBarTexts.style.marginTop = "0px";
   contenidoPrincipal.style.marginTop = "0px";
   sectionTitle.textContent = "LIBROS";
-  sectionTitle.style.marginTop = "100px"
-  sectionTitle.style.marginLeft = "93px"
-  books.textContent = "PERSONAJES";
-  templateBooks(dataBooks)
+  sectionTitle.style.marginTop = "100px";
+  sectionTitle.style.marginLeft = "93px";
+  templateBooks(dataBooks);
 });
 
 ////CHANGE TO s.DATOS CURIOSOS
@@ -110,32 +124,37 @@ function templateFunFacts(cardfacts) {
 templateFunFacts(dataFacts);
 
 facts.addEventListener("click", function (e) {
-  e.preventDefault(); // prevenir la acción por defecto del enlace
+  e.preventDefault();
   charactersSection.style.display = "none";
   imagenPrincipal.style.display = "none";
   booksSection.style.display = "none";
   buttons.style.display = "none";
   booksSection.style.display = "none";
   funFactssection.style.display = "block";
-  btncalcular.style.display = "block"
+  btncalcular.style.display = "block";
   imgHeader.style.display = "block";
+  calculatedFunfact.style.display = "block";
   imgHeader.style.marginTop = "0px";
   navBarTexts.style.marginTop = "0px";
   contenidoPrincipal.style.marginTop = "0px";
   sectionTitle.textContent = "DATOS CURIOSOS";
-  sectionTitle.style.marginTop = "100px"
-  sectionTitle.style.marginLeft = "93px"
-  facts.textContent = "PERSONAJES";
-
-  templateBooks(dataBooks)
+  sectionTitle.style.marginTop = "100px";
+  sectionTitle.style.marginLeft = "93px";
+  templateBooks(dataFacts);
 });
 
-//CALCULATE
-btncalcular.addEventListener("click", function () {
-  const respuestaCalcular = document.getElementById('respuestaDatoCurioso');
-  respuestaCalcular.style.display = "block";
-  const data = calcular(dataCharacters, 'Female');
-  const respuesta = document.getElementById("respuestaDatoCurioso");
-  respuesta.innerHTML = "Existen " + data + " personajes femeninos";
-
+//GO BACK TO CHARACTERS
+imgHeader.addEventListener("click", function (p) {
+  p.preventDefault();
+  btncalcular.style.display = "none";
+  booksSection.style.display = "none";
+  funFactssection.style.display = "none";
+  charactersSection.style.display = "block";
+  imagenPrincipal.style.display = "block";
+  imgHeader.style.display = "block";
+  buttons.style.display = "block";
+  imgHeader.style.marginTop = "-595px";
+  navBarTexts.style.marginTop = "-595px";
+  contenidoPrincipal.style.marginTop = "435px";
+  sectionTitle.textContent = "PERSONAJES";
 });
